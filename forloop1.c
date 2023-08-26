45cc672583e3ed7a4c92e9d255bf45023d162a6c
rere <122435948+Rehemashammah@users.noreply.github.com>
2023-08-26 23:35:07 +0300

    Update forloop1.c

---
+++ forloop1.c
@@ -0,0 +1,13 @@
+#include<sdio.h>
+int main()
+{
+  /*When you know exactly how many times you want to loop through a block of code, 
+  use for loop instead of while loop*/
+  int i;
+for (i=0; i<=5; i++)
+{
+printf("%d\n", i);
+}
+}
+  
+
